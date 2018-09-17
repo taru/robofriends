a8d046d0b9343d6f3ee2efc218da5cd45ebb69fd
taru <39840910+taru@users.noreply.github.com>
2018-09-17 01:28:38 -0400

    adding scrollbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
             .then(response=> response.json())
             .then(users => this.setState({ robots: users }))
     }
-// we don't use arrow functions here because this is part of react
+// note that since componentDidMount is built into React, we omit the arrow function syntax
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
@@ -36,7 +37,9 @@ class App extends Component {
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filterRobots}/>
+                    <Scroll>
+                        <CardList robots={filterRobots}/>
+                    </Scroll>
                 </div>
             )
         }
---
+++ src/Scroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY: 'scroll', border: '3px solid black', height: '800px'}}>
+            {props.children}
+        </div>
+        // this styles the children of Scroll so we can scroll down without losing searchbar. overflow-y property specifies whether to clip the content, add a scroll bar, or display overflow content of a block-level element, when it overflows at the top and bottom edges
+    )
+};
+
+export default Scroll;
